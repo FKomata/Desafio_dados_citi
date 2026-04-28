1c7eb5737acabaccf1951cef567310d07e8fab34
Felipe Komata <fbkomata@gmail.com>
2026-04-28 18:28:03 -0300

    tratamento de CPF

--- dados.py
+++ dados.py
@@ -35,7 +35,7 @@ print("===============================")
 
 
 #topico 3.
-#tirar linahs duplicadas
+#tirar linhas duplicadas
 
 df = df.drop_duplicates()
 
@@ -44,3 +44,26 @@ print("===============================")
 print("quantidade de linhas e colunas após remoção de duplicadas")
 print("Shape: ", df.shape)
 print("===============================")
+
+#topico 4.
+#padronização do CPF
+df["CPF_Cliente"] = df["CPF_Cliente"].str.strip()
+df["CPF_Cliente"] = df["CPF_Cliente"].str.replace(r'[.-]', '', regex=True)
+#tirar os . e - para garantir que todos os cpf vao se comportar igual
+df["CPF_Cliente"] = df["CPF_Cliente"].fillna("Não informado")
+df["CPF_Cliente"] = df["CPF_Cliente"].str.title()
+#indicar a parte nao informada 
+
+#filtro para o .loc saber o tamanho da string que ele deve mexer
+numerosseparados = (df['CPF_Cliente'].str.len() == 11)
+
+#mudanças que indicam onde dee ser otado o . e a -
+df.loc[numerosseparados,"CPF_Cliente"] = (
+    df.loc[numerosseparados,"CPF_Cliente"].str[:3] + "." + df.loc[numerosseparados,"CPF_Cliente"].str[3:6] + "." + df.loc[numerosseparados,"CPF_Cliente"].str[6:9] +"-"+ df.loc[numerosseparados,"CPF_Cliente"].str[9:]
+)
+
+
+#salvamento do tratamento de dados
+#df.to_csv("basededadoslimpa.csv",index=False)
+print(df["CPF_Cliente"].head())
+
