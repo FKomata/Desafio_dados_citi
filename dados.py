b6d8e0a16bd08c3eef976e31aa0dc257da5620e2
Felipe Komata <fbkomata@gmail.com>
2026-04-29 09:57:50 -0300

    adição do tratamento de valores e padronizaçao da moeda e tipo de transação

--- dados.py
+++ dados.py
@@ -81,6 +81,78 @@ df["Data_Transacao"] = df["Data_Transacao"].dt.strftime("%Y-%m-%d")
 
 print(df["Data_Transacao"].head(100))
 
+#topico 6
 
+def convertevalor(linha):
+    moeda = linha["Moeda"]
+    valor = linha["Valor_Transacao"]
+    
+    if moeda == "USD":
+        return valor * 5.00
+    elif moeda == "EUR":
+        return valor * 5.50
+    elif moeda == "GBP":
+        return valor * 6.30
+    else:
+        return valor
+    
+#tratamento do codigo tirando os R$ USD EUR GPB e utilização do astype(str) para transforma tudo da coluna em string 
+df["Valor_Transacao"] = df["Valor_Transacao"].str.strip()
+df["Valor_Transacao"] = df["Valor_Transacao"].astype(str).str.replace('R$', '', regex=False)
+df["Valor_Transacao"] = df["Valor_Transacao"].str.replace(r'[GPB]', '', regex=True)
+df["Valor_Transacao"] = df["Valor_Transacao"].str.replace(r'[EUR]', '', regex=True)
+df["Valor_Transacao"] = df["Valor_Transacao"].str.replace(r'[USD]', '', regex=True)
+
+#transforma em float tirando a , por .
+df["Valor_Transacao"] = df["Valor_Transacao"].str.replace(r'[.]', '', regex=True)
+df["Valor_Transacao"] = df["Valor_Transacao"].str.replace(r'[,]', '.', regex=True)
+
+df["Valor_Transacao"] = pd.to_numeric(df["Valor_Transacao"], errors='coerce')
+
+df["Valor_Transacao"] = df.apply(convertevalor, axis=1)
+
+df.loc[df["Valor_Transacao"] <= 0 , "Valor_Transacao"] = None
+
+
+mediana = df["Valor_Transacao"].median()
+df["Valor_Transacao"] = df["Valor_Transacao"].fillna(mediana)
+df["Valor_Transacao"] = df["Valor_Transacao"].round(2)
+
+df["Moeda"] = "BRL"
+
+
+print(mediana)
+print(df["Moeda"].head(100))
+print(df["Valor_Transacao"].head(100))
+
+#topico 7
+mapa_tipo = {
+    'transferencia': 'Transferência',
+    'transferência': 'Transferência',
+    'transf.': 'Transferência',
+    'pgto.': 'Pagamento',
+    'pagamento': 'Pagamento',
+    'pix': 'PIX',
+    'p.i.x': 'PIX',
+    'pix': 'PIX',
+    'pixe': 'PIX',
+    'depósito': 'Depósito',
+    'deposito': 'Depósito',
+    'saque': 'Saque',
+    'retirada': 'Saque',
+    'boleto' : 'Pagamento',
+    'ted' : 'Transferência',
+    'doc' :  'Transferência'
+}
+
+df['Tipo_Transacao'] = (
+    df['Tipo_Transacao']
+    .str.strip()
+    .str.lower()
+    .map(mapa_tipo)
+    .fillna(df['Tipo_Transacao'].mode()[0])
+)
+
+print(df["Tipo_Transacao"].head())
 #salvamento do tratamento de dados
 #df.to_csv("basededadoslimpa.csv",index=False)
